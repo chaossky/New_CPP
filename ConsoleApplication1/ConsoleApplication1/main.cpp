ae1fadb56293279429c3d3c61385397312c5f4d5
이장훈 <chaossky@naver.com>
2017-04-20 08:46:37 +0900

    함수의 반환형을 void 로 변환

--- ConsoleApplication1/ConsoleApplication1/main.cpp
+++ ConsoleApplication1/ConsoleApplication1/main.cpp
@@ -1,10 +1,15 @@
+//기본적으로 사용할 수 있는 라이브러리를 참조한다.
 #include <iostream>
+//네임 스페이스를 정해서 사용할 것들을 더한다.
 using namespace std;
-
-int main() {
+//기본적으로 사용되는 main()함수 또는 메소드를 정의한다.
+// 메소드는 기본적으로 열번 "Hello "문장을 출력하도록 한다.
+// 함수의 반환값은 정수형으로 지정한다.
+void main() {
 	for(int i=0;i<10;i++){
+
 		cout << "Hello this is my game." << endl;
 	}
 	
-	return 0;
+	return ;
 }

6098861ff515d081cfb9dc3b6c38ea72d5510546
이장훈 <chaossky@naver.com>
2017-04-25 08:26:07 +0900

    주석 첨가

--- Function/Function/function.cpp
+++ Function/Function/function.cpp
@@ -1,14 +1,21 @@
 // function example
+// 함수를 만들어 보자
+//입출력 스트림 파일을 인쿠르드 하고
 #include <iostream>
+// 네임 스페이스를 스탠다드로 ㅋㅋ
 using namespace std;
 
+
+//두개의 숫자를 더하는 함수
 int addition(int a, int b)
 {
+
 	int r;
 	r = a + b;
 	return r;
 }
 
+//두개의 숫자 중에서 큰 숫자에서 작은 숫자를 빼는 함수를 정의 한다.
 int subtraction(int x, int y)
 {
 	int z;

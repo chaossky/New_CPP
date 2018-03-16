6589c1fb755a1baa098fa812f96e87d91f4fe9d8
Janghoon Lee <chaossky@naver.com>
2018-03-17 07:28:39 +0900

    조금 바뀜

--- ConsoleApplication2/소스.cpp
+++ ConsoleApplication2/소스.cpp
@@ -1,37 +1,18 @@
-#include <GLFW/glfw3.h>
+#include <iostream>
 
-int main(void)
-{
-	GLFWwindow* window;
+using namespace std;
 
-	/* Initialize the library */
-	if (!glfwInit())
-		return -1;
-
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window)
-	{
-		glfwTerminate();
-		return -1;
+int main() {
+	int i, S;
+	i = 0;
+	S = 0;
+	while (i < 100) {
+		S = S + i;
+		i = i + 2;
+		cout << "i=" << i << "   ÃÑÇÕÀº" << S << endl;
 	}
+	
+	cout << "Â¦¼öÀÇ ÇÕ " <<S<< endl;
 
-	/* Make the window's context current */
-	glfwMakeContextCurrent(window);
-
-	/* Loop until the user closes the window */
-	while (!glfwWindowShouldClose(window))
-	{
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
 	return 0;
 }
